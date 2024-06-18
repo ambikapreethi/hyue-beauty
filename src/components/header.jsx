cff06913284857a0ed72acb325f49069f23e7003
Ambika Intern <ambika.doodleblue@gmail.com>
2024-06-18 16:13:32 +0530

    second commit

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/header.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars,faSearch,faShoppingBag,faUser } from '@fortawesome/free-solid-svg-icons';
 import { Link } from "react-router-dom";
+
 function Header()
 {
-    return(
+ const[showSideBar,setshowSideBar]=useState(false);
+ const handleSideBar=()=>
+ {
+    setshowSideBar("true");
+ }
+ const closeSideBar=()=>
+    {
+       setshowSideBar("false");
+    }
+     return(
+       
         <div>
             <nav className="menuBar">
-                        <a><FontAwesomeIcon icon={faBars} className="faBars" />
+                           {showSideBar==="true" && (<div class="sidebar">
+                            <button style={{fontSize:"15px",border:"1px solid grey",height:"20px"}} onClick={closeSideBar}>close</button>
+                                    <Link to="/"><p>SHOP ALL</p></Link>
+                                    <Link to="/hyue-lipsticks"><p>HYUE HYDRAMATTE<sup>TM</sup>LIQUID LIPSTICK</p></Link>
+                                    <Link to="/hyue-nailpolish"><p>HYUE GEL FX Nail Paint</p></Link>
+                                    <p>HYUE Chrome Glazed Nail Paint</p>
+                                    
+                            </div>)}
+                        <a><FontAwesomeIcon icon={faBars} className="faBars" onClick={handleSideBar}/>
                         <span className="icon"><FontAwesomeIcon icon={faSearch} className="faSearch" /></span></a>
                         <a><p style={{fontSize:"30px",fontWeight:"normal",letterSpacing:"2px",color:"black",marginTop:"-10px"}}>HYUE</p></a>
                         <a><Link to="/login"><FontAwesomeIcon icon={faUser} className="faUser"/> </Link>
